e369f2117cf11128293997b8f5b4553ff6b6626b
Chang-Goo Kim <cgkim@astro.princeton.edu>
2017-09-15 14:07:23 -0400

    Added projection plot with yt

---
+++ pyathena/yt_analysis/plot_projection.py
@@ -0,0 +1,87 @@
+import matplotlib as mpl
+#mpl.use('Agg')
+
+import pyathena.yt_analysis.ytathena as ya
+import yt
+import glob
+import argparse
+import os
+import cPickle as pickle
+
+import matplotlib.colorbar as colorbar
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+from matplotlib.colors import LogNorm,SymLogNorm,NoNorm,Normalize
+from pyathena import read_starvtk,texteffect,set_units
+import numpy as np
+import string
+from scatter_sp import scatter_sp
+
+aux=ya.set_aux('solar')
+
+def plot_projection(surfname,f):
+    global aux
+
+    plt.rc('font',size=11)
+    plt.rc('xtick',labelsize=11)
+    plt.rc('ytick',labelsize=11)
+
+    fig=plt.figure(0,figsize=(5.5,5))
+    gs = gridspec.GridSpec(2,2,width_ratios=[1,0.03],wspace=0.0)
+
+    sp= read_starvtk(f[:-3]+'starpar.vtk')
+    frb=pickle.load(open(surfname,'rb'))
+    extent=np.array(frb['bounds'])/1.e3
+    x0=extent[0]
+    y0=extent[2]
+    Lx=extent[1]-extent[0]
+    Lz=extent[3]-extent[2]
+ 
+    if frb.has_key('time'):
+        tMyr=frb['time']
+    else:
+        time,sp=read_starvtk(f[:-3]+'starpar.vtk',time_out=True)
+        tMyr=time*Myr
+    ax=plt.subplot(gs[:,0])
+    im=ax.imshow(frb['data'],norm=LogNorm(),origin='lower')
+    im.set_extent(extent)
+    im.set_cmap(aux['surface_density']['cmap'])
+    im.set_clim(aux['surface_density']['clim'])
+    ax.text(extent[0]*0.9,extent[3]*0.9,
+            't=%3d Myr' % tMyr,ha='left',va='top',**(texteffect()))
+
+    scatter_sp(sp,ax,axis='z',runaway=True,type='surf')
+
+    cax=plt.subplot(gs[0,1])
+    cbar = fig.colorbar(im,cax=cax,orientation='vertical')
+    cbar.set_label(aux['surface_density']['label'])
+
+    cax=plt.subplot(gs[1,1])
+    cbar = colorbar.ColorbarBase(cax, ticks=[0,20,40],
+           cmap=plt.cm.cool_r, norm=Normalize(vmin=0,vmax=40), 
+           orientation='vertical')
+    cbar.set_label(r'${\rm age [Myr]}$')
+
+    norm_factor=2.
+    s1=ax.scatter(Lx*2,Lz*2,
+      s=np.sqrt(1.e3)/norm_factor,color='k',
+      alpha=.8,label=r'$10^3 M_\odot$')
+    s2=ax.scatter(Lx*2,Lz*2,
+      s=np.sqrt(1.e4)/norm_factor,color='k',
+      alpha=.8,label=r'$10^4 M_\odot$')
+    s3=ax.scatter(Lx*2,Lz*2,
+      s=np.sqrt(1.e5)/norm_factor,
+      color='k',alpha=.8,label=r'$10^5 M_\odot$')
+
+    ax.set_xlim(x0,x0+Lx)
+    ax.set_ylim(y0,y0+Lz);
+    legend=ax.legend((s1,s2,s3),(r'$10^3 M_\odot$',r'$10^4 M_\odot$',r'$10^5 M_\odot$'), 
+                      loc=2,ncol=3,bbox_to_anchor=(0.0, 1.15),
+                      fontsize='medium',frameon=True)
+
+    ax.set_xlabel('x [kpc]')
+    ax.set_ylabel('y [kpc]')
+
+    pngfname=surfname[:-1]+'png'
+    return fig
---
+++ pyathena/yt_analysis/scatter_sp.py
@@ -0,0 +1,76 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from pyathena import read_starvtk,texteffect,set_units
+def projection(sp,axis):
+    if axis == 0 or axis == 'z':
+        spx=sp['x1']
+        spy=sp['x2']
+        spz=sp['x3']
+    elif axis == 1 or axis == 'y':
+        spx=sp['x1']
+        spy=sp['x3']
+        spz=sp['x2']
+    elif axis == 2 or axis == 'x':
+        spx=sp['x2']
+        spy=sp['x3']
+        spz=sp['x1']
+    return spx,spy,spz
+
+def projection_v(sp,axis):
+    if axis == 0 or axis == 'z':
+        spx=sp['v1']
+        spy=sp['v2']
+        spz=sp['v3']
+    elif axis == 1 or axis == 'y':
+        spx=sp['v1']
+        spy=sp['v3']
+        spz=sp['v2']
+    elif axis == 2 or axis == 'x':
+        spx=sp['v2']
+        spy=sp['v3']
+        spz=sp['v1']
+    return spx,spy,spz
+
+def scatter_sp(sp,ax,axis=0,thickness=10,norm_factor=4., \
+  type='slice',kpc=True,runaway=True):
+    unit=set_units(muH=1.4271)
+    Msun=unit['mass'].to('Msun').value
+    Myr=unit['time'].to('Myr').value
+    #print len(sp)
+    if len(sp) >0:
+      runaways=(sp['mass'] == 0.0)
+      sp_runaway=sp[runaways]
+      sp_normal=sp[-runaways]
+      #print len(sp_runaway)
+      if len(sp_runaway) > 0 and runaway:
+        spx,spy,spz=projection(sp_runaway,axis)
+        spvx,spvy,spvz=projection_v(sp_runaway,axis)
+        if kpc:
+            spx = spx/1.e3
+            spy = spy/1.e3
+        if type == 'slice': 
+            islab=np.where(abs(spz) < thickness)
+            #ax.scatter(spx.iloc[islab],spy.iloc[islab],marker='.',c='k',alpha=1.0)
+            #ax.quiver(spx.iloc[islab],spy.iloc[islab],
+            #      spvx.iloc[islab],spvy.iloc[islab],color='k',alpha=1.0)
+        ax.scatter(spx,spy,marker='o',color='k',alpha=1.0,s=10.0/norm_factor)
+        #ax.quiver(spx,spy,spvx,spvy,color='w',alpha=0.5)
+ 
+      if len(sp_normal) > 0: 
+        spx,spy,spz=projection(sp_normal,axis)
+        if kpc:
+            spx = spx/1.e3
+            spy = spy/1.e3
+        if type == 'slice': xbool=abs(spz) < thickness
+        spm=np.sqrt(sp_normal['mass']*Msun)/norm_factor
+        spa=sp_normal['age']*Myr
+        iyoung=np.where(spa < 40.)
+        #print len(iyoung[0])
+        if type == 'slice':
+            islab=np.where(xbool*(spa<40))
+            ax.scatter(spx.iloc[islab],spy.iloc[islab],marker='o',\
+                s=spm.iloc[islab],c=spa.iloc[islab],\
+                vmax=40,vmin=0,cmap=plt.cm.cool_r,alpha=1.0)
+        ax.scatter(spx.iloc[iyoung],spy.iloc[iyoung],marker='o',\
+            s=spm.iloc[iyoung],c=spa.iloc[iyoung],\
+            vmax=40,vmin=0,cmap=plt.cm.cool_r,alpha=0.7)
