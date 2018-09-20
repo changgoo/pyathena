210c59827264c5707cdfeadc2bddb4ecd74f25c7
Chang-Goo Kim <changgoo@princeton.edu>
2018-09-20 16:08:23 -0400

    Revise draw script

--- scripts/draw_slice_surf.py
+++ scripts/draw_slice_surf.py
@@ -1,17 +1,40 @@
 import matplotlib as mpl
 mpl.use('agg')
 
-import glob,os
+import glob
+import sys,os,shutil
+import pandas as pd
+
+sys.path.insert(0,'../')
 from pyathena.plot_tools import plot_slices,plot_projection,set_aux
 from pyathena.utils import compare_files
 
+narg=len(sys.argv)
+system='tigress'
 base='/tigress/changgoo/'
-dirs=glob.glob('{}/*acc*/hst'.format(base))
+if narg > 1:
+    system = sys.argv[1]
+    if system == 'pleiades':
+        base='/u/ckim14/'
+    elif system =='tigress':
+        base='/tigress/changgoo/'
+    else:
+        print '{} is not supported'.format(system)
+        sys.exit()
+if narg > 2:
+    dirs=glob.glob('{}/{}'.format(base,sys.argv[2]))
+else:
+    dirs=glob.glob('{}/*'.format(base))
 ids=[]
+do_pickling = False
 for dd in dirs:
-    ids.append(dd.split('/')[-2])
-print ids
-ids=['R4_4pc_rst_newacc']
+    if os.path.isdir(dd):
+        if os.path.isdir(dd+'/id0/'):
+            do_pickling = True
+        else:
+            do_pickling = False
+        if os.path.isdir(dd+'/hst/'):
+            ids.append(os.path.basename(dd))
 
 for pid in ids:
     print pid
