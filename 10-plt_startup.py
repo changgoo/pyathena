50aae4f282b85f06cd1926b4bead282e7a523038
Chang-Goo Kim <changgoo@princeton.edu>
2019-03-16 19:12:11 -0400

    New plt default

---
+++ 10-plt_startup.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+plt.rcParams['figure.figsize']=(10,6)
+plt.rcParams['font.size']=20
+plt.rcParams['lines.linewidth']=3
+plt.rcParams['axes.linewidth']=3
+plt.rcParams['xtick.major.width']=3
+plt.rcParams['ytick.major.width']=3
+plt.rcParams['xtick.major.size']=5
+plt.rcParams['ytick.major.size']=5
